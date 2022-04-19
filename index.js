b2fa799bbc2c8abd117da8365898fb8dcead4f64
Santiago Sanmartín <saureth001@gmail.com>
2022-04-19 12:41:48 -0500

    [FEAT] Server instance created

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./src/typedefs";
+import resolvers from "./src/resolvers";
 
-// const typeDefs = gql`
-//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-//   # This "Book" type defines the queryable fields for every book in our data source.
-//   type Book {
-//     title: String
-//     author: String
-//   }
-
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     books: [Book]
-//   }
-// `;
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
