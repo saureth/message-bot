6486590acfcb8fa37a8b03c2a2a7c0b26cbb2b6e
Santiago Sanmartín <saureth001@gmail.com>
2022-04-19 12:31:28 -0500

    [FEAT] Create a new typedef for users with collection and query

---
+++ src/typedefs/library/typeDefUser.js
@@ -0,0 +1,16 @@
+import { gql } from "apollo-server";
+
+const typeDefUser = gql`
+
+    type User {
+        id: number
+        fullname: string
+        email: string
+        pass: string
+        usertype: number
+    }
+
+    type Query {
+        getUsers: [User]
+    }
+`;
