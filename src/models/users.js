8ddffa41f040fdcc1f046c0d5db6b67dc0240fd6
Santiago Sanmartín <saureth001@gmail.com>
2022-04-19 15:01:47 -0500

    [FEAT] Creation of user model

---
+++ src/models/users.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema(
+    {
+      fullname: String,
+      email: String,
+      pass: String,
+      usertype: Number
+      //usertype: { type: mongoose.ObjectId, ref: "Company" },
+    },
+    { collection: "users" }
+);
+
+export default mongoose.model("users", Schema)
