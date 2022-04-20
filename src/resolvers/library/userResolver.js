5a0b8ad65c92105d978d05e06a32c4f922bb0fd7
Santiago Sanmartín <saureth001@gmail.com>
2022-04-20 16:16:48 -0500

    [REF] User query populates messages and categories

--- src/resolvers/library/userResolver.js
+++ src/resolvers/library/userResolver.js
@@ -3,7 +3,15 @@ import Users from "../../models/users.js";
 const userResolver = {
     Query: {
         getUsers: async () => {
-            const res = await Users.find().populate('messageList');
+            const res = await Users.find()
+                .populate('messageList')
+                .populate({
+                    path: 'messageList',
+                    populate: {
+                      path: 'category',
+                      model: 'Categories'
+                    }
+                })
             return res;
         }
     },
